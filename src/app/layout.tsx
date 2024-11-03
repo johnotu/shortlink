d99c5aa0f838992b5fec6e312ad8ade9cab2c461
John Otu <johnotu@users.noreply.github.com>
2024-11-03 04:59:53 +0100

    bootstrap and home screen
    - added bootstrap to make things look nice
    - added home screen with a simple way to input and shorten a url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,8 +27,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} d-flex h-100 text-center `}
+      >
+        <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+          <header className="mb-5">
+            <div>
+              <Link className="h3 float-start mb-0" href="/">
+                ShortLink
+              </Link>
+              <nav className="nav nav-masthead justify-content-center float-end">
+                <Link
+                  className={`nav-link fw-bold py-1 px-0 active`}
+                  href="/links"
+                >
+                  My Links
+                </Link>
+              </nav>
+            </div>
+          </header>
+          {children}
+          <footer className="mt-auto text-white-50">
+            <p>
+              Powered by{" "}
+              <a href="https://nextjs.org/" className="text-white">
+                Next.js
+              </a>
+              ,{" "}
+              <a href="https://getbootstrap.com/" className="text-white">
+                Bootstrap
+              </a>
+              .
+            </p>
+          </footer>
+        </div>
       </body>
     </html>
   );
